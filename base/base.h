4325332bd3a06fd3328f1c976e16e632bc8121b3
owen <thss15_zhangqr@163.com>
2017-12-23 14:19:11 +0800

    clean repo

--- base/base.h
+++ base/base.h
@@ -4,8 +4,8 @@
 enum {
     RADIO_QUEUE_LEN = 30,
     UART_QUEUE_LEN = 30,
-    AM_SENSORMSG = 0x93,
-	TIMER_PERIOD_MILLI = 1000,
+    // TODO:change these settings
+    AM_SENSORMSG = 0x93, 
 	AM_SENSOR_TO_SENSOR = 6,
 	AM_BASE_TO_SENSORS = 10,
 	AM_BASE_TO_PC = 11
@@ -13,19 +13,16 @@ enum {
 
 typedef nx_struct SensorMsg 
 {
-	nx_uint16_t version;
-	nx_uint16_t interval;
-	nx_uint16_t node_id;
+	nx_uint16_t version;  //包的版本
+	nx_uint16_t interval; // 采样间隔
+	nx_uint16_t node_id;  
 	nx_uint16_t sequence_number;
 	nx_uint16_t temperature;
 	nx_uint16_t humidity;
 	nx_uint16_t light_intensity;
 	nx_uint32_t current_time;
 } SensorMsg;
-/*
-typedef nx_struct FreqMsg {
-	nx_uint16_t freq;
-} FreqMsg;*/
+
 
 
 #endif
